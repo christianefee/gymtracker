6712a2cbacfe7014903f22f3360b497ca7e3c830
Christian <kikito3107@gmail.com>
2026-03-29 15:36:41 -0500

    def para cargar y guardar en JSON

--- main.py
+++ main.py
@@ -10,4 +10,19 @@ from logica.equilibrio import AnalizadorEquilibrio
 ARCHIVO_USUARIOS: str = "datos/usuarios.json"
 ARCHIVO_SESIONES: str = "datos/sesiones.json"
 
-# Cambio
+def cargar_usuarios() -> dict:
+    with open(ARCHIVO_USUARIOS, "r") as archivo:
+        return json.load(archivo)
+
+def guardar_usuarios(usuarios: dict) -> None:
+    with open(ARCHIVO_USUARIOS, "w") as archivo:
+        json.dump(usuarios, archivo, indent=4)
+
+def cargar_sesiones() -> list:
+    with open(ARCHIVO_SESIONES, "r") as archivo:
+        return json.load(archivo)
+
+def guardar_sesiones(sesiones: list) -> None:
+    with open(ARCHIVO_SESIONES, "w") as archivo:
+        json.dump(sesiones, archivo, indent=4)
+
